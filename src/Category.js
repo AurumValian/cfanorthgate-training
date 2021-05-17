3ef645b8d99ab190d2fd25eb6e79d1e9de82563c
Ben McClung <aurumvalian@gmail.com>
2021-05-17 11:58:10 -0600

    Create basic first page; add CFA red hexcode

---
+++ src/Category.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Category.css"
+
+function Category(props) {
+  return (
+    <button className="category">
+      COMING SOON
+    </button>
+  )
+}
+
+export default Category;
