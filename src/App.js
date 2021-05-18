cb2d4b30b25116bf745afe62a027035304fe1501
Ben McClung <aurumvalian@gmail.com>
2021-05-17 18:48:28 -0600

    Add main page styling and change App to class component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Category from "./Category"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Chick-Fil-A
-      </header>
-      <section className="category-buttons">
-        <Category />
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      page: "main",
+      type: ""
+    }
+
+    this.toSubcategories = this.toSubcategories.bind(this);
+  }
+
+  toSubcategories(event) {
+    this.setState({page: "sub", type: event.target.innerText})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Chick-Fil-A
+        </header>
+        <section className="category-buttons">
+          <Category type="Biscuits" toSubcategories={this.toSubcategories} color="blue"/>
+          <Category type="Prep" toSubcategories={this.toSubcategories}/>
+          <Category />
+          <Category />
+          <Category />
+          <Category />
+        </section>
+        {this.state.page === "sub" && (
+          <section>
+            {this.state.type}
+          </section>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Category.js
+++ src/Category.js
@@ -2,9 +2,11 @@ import React from 'react';
 import "./Category.css"
 
 function Category(props) {
+  let className = "category ";
+  className += props.type
   return (
-    <button className="category">
-      COMING SOON
+    <button className={className} onClick={props.toSubcategories} color={props.color}>
+      {props.type || "COMING SOON"}
     </button>
   )
 }
